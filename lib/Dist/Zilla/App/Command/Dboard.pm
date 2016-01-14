573480ca3c865ab75546dad443a9f103dbe9524e
Erik Carlsson <info@code301.com>
2016-01-14 13:40:57 +0000

    Init

---
+++ lib/Dist/Zilla/App/Command/Dboard.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package Dist::Zilla::App::Command::Dboard;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::App::Command::Dboard;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::App::Command::Dboard is ...
+
+=head1 SEE ALSO
+
+=cut
